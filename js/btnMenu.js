c3fa13888d1c8ea98cb0fc5c83f5f9c8bef86113
Rute Miliana <rutemiliana13@gmail.com>
2022-01-14 09:28:22 -0300

    correção de links

--- js/btnMenu.js
+++ js/btnMenu.js
@@ -1,14 +1,12 @@
-const $menu = document.querySelector('.menu')
-const $btnMenuOpen = document.querySelector('.btnMenu_open') /*usar $ para variavel que guarda referencia do html */
-$btnMenuOpen.addEventListener('click' , function(){
-    $menu.classList.add('menu_open'); /*adiciona na classe menu o menu_open
+
+/*usar $ para variavel que guarda referencia do html */
+document.querySelector('.btnMenu_open').addEventListener('click' , function(){
+    document.querySelector('.menu').classList.add('menu_open'); /*adiciona na classe menu o menu_open
     classList é para manipular lista de classe*/ 
 })
 
-const $btnMenuClose = document.querySelector('.btnMenu_close')
-$btnMenuClose.addEventListener('click' , function(){
-    $menu.classList.remove('menu_open') /*remove da classe menu o menu_open
+document.querySelector('.btnMenu_close').addEventListener('click' , function(){
+    document.querySelector('.menu').classList.remove('menu_open') /*remove da classe menu o menu_open
     classList é para manipular lista de classe*/ 
 })
 
-/*Lembrar: tirar as const para evitar consumo de memória */
